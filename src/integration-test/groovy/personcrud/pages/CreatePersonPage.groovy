0f7f3f50b4f4efbc67c2295db237afa6ab50c132
Sergio del Amo <sergio.delamo@softamo.com>
2019-12-18 10:27:25 +0100

    use geb modules for input fields

--- src/integration-test/groovy/personcrud/pages/CreatePersonPage.groovy
+++ src/integration-test/groovy/personcrud/pages/CreatePersonPage.groovy
@@ -1,6 +1,7 @@
 package personcrud.pages
 
 import geb.Page
+import geb.module.TextInput
 
 class CreatePersonPage extends Page {
 
@@ -12,15 +13,15 @@ class CreatePersonPage extends Page {
 
     static content = {
         submitButton { $('#create', 0) }
-        firstNameInputField { $('#firstName', 0) }
-        lastNameInputField { $('#lastName', 0) }
+        firstNameInputField { $('#firstName', 0).module(TextInput) }
+        lastNameInputField { $('#lastName', 0).module(TextInput) }
         ageInputField { $('#age', 0) }
         errorMessage { $('li', 'data-field-id':'age') }
     }
 
     void populateCreatePersonForm(String firstName, String lastName, String age) {
-        firstNameInputField << firstName
-        lastNameInputField << lastName
+        firstNameInputField.text = firstName
+        lastNameInputField.text = lastName
         ageInputField << age
         submitButton.click()
     }
